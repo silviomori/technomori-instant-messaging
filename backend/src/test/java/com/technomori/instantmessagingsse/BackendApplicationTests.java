1c8aa80e27db6af8657c9d23d3cfc4d635db0c6a
Silvio Mori <silviomori@gmail.com>
2023-04-05 15:31:00 -0500

    Create backend project using Spring Boot

---
+++ backend/src/test/java/com/technomori/instantmessagingsse/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technomori.instantmessagingsse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
